5b2797a8d2e18f2e2df4e574c4ee2ea73503e245
Stanly Johnson <stanly@sovereignwallet.network>
2019-05-17 15:21:45 +0900

    added xlm and xrp

--- scripts/getPrices.js
+++ scripts/getPrices.js
@@ -6,10 +6,15 @@ document.addEventListener("DOMContentLoaded", function() {
       chrome.tabs.sendMessage(tabs[0].id, { message: "clicked_scan_button" }, function(response) {
         document.getElementById("message").textContent = "";
         valueArray = response.data;
+        console.log(valueArray);
         btc_value = valueArray.usd.value;
         eth_value = btc_value / valueArray.eth.value;
+        xlm_value = btc_value / valueArray.xlm.value;
+        xrp_value = btc_value / valueArray.xrp.value;
         document.getElementById("btc_rate").textContent = `1BTC = ${btc_value} USD`;
         document.getElementById("eth_rate").textContent = `1ETH = ${eth_value} USD`;
+        document.getElementById("xrp_rate").textContent = `1XRP = ${xrp_value} USD`;
+        document.getElementById("xlm_rate").textContent = `1XLM = ${xlm_value} USD`;
       });
     });
   });
