ffb2b182292538f4f3a91df20b0da5f0bd909c27
Stanly Johnson <stanlyjohnson@outlook.com>
2019-06-02 14:38:59 +0530

    console logs removed

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.message === 'clicked_scan_button') {
-    console.log('Fetching Prices from CoinGecko');
     var req = new XMLHttpRequest();
     req.open('GET', 'https://api.coingecko.com/api/v3/exchange_rates', true);
     req.onload = onResponseReceived;
@@ -8,8 +7,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     async function onResponseReceived() {
       let requestJSON = await JSON.parse(req.response);
       let valueArray = requestJSON.rates;
-      console.log(valueArray);
-      console.log('Rate Fetched');
       sendResponse({ data: valueArray, success: true });
     }
     return true;
