e77507967871ff741ed2d3ef748480c92d26fe36
Stanly Johnson <stanlyjohnson@outlook.com>
2019-06-02 14:12:10 +0530

    Button removed
    Button to fetch price removed, the rates are fetched background

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,15 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === "clicked_scan_button") {
-    console.log("Fetching Prices from CoinGecko");
+  if (request.message === 'clicked_scan_button') {
+    console.log('Fetching Prices from CoinGecko');
     var req = new XMLHttpRequest();
-    req.open("GET", "https://api.coingecko.com/api/v3/exchange_rates", true);
+    req.open('GET', 'https://api.coingecko.com/api/v3/exchange_rates', true);
     req.onload = onResponseReceived;
     req.send();
     async function onResponseReceived() {
       let requestJSON = await JSON.parse(req.response);
       let valueArray = requestJSON.rates;
       console.log(valueArray);
-      console.log("Rate Fetched");
+      console.log('Rate Fetched');
       sendResponse({ data: valueArray, success: true });
     }
     return true;
--- scripts/getPrices.js
+++ scripts/getPrices.js
@@ -1,21 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var button = document.getElementById("scan");
-  button.addEventListener("click", function() {
-    document.getElementById("message").textContent = `Fetching prices from coingecko`;
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { message: "clicked_scan_button" }, function(response) {
-        document.getElementById("message").textContent = "";
-        valueArray = response.data;
-        console.log(valueArray);
-        btc_value = valueArray.usd.value;
-        eth_value = parseFloat(btc_value / valueArray.eth.value).toFixed(4);
-        xlm_value = parseFloat(btc_value / valueArray.xlm.value).toFixed(4);
-        xrp_value = parseFloat(btc_value / valueArray.xrp.value).toFixed(4);
-        document.getElementById("btc_rate").textContent = `1BTC = ${btc_value} USD`;
-        document.getElementById("eth_rate").textContent = `1ETH = ${eth_value} USD`;
-        document.getElementById("xrp_rate").textContent = `1XRP = ${xrp_value} USD`;
-        document.getElementById("xlm_rate").textContent = `1XLM = ${xlm_value} USD`;
-      });
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('scan');
+
+  document.getElementById('message').textContent = `Fetching prices from coingecko`;
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { message: 'clicked_scan_button' }, function(response) {
+      document.getElementById('message').textContent = '';
+      valueArray = response.data;
+      console.log(valueArray);
+      btc_value = valueArray.usd.value;
+      eth_value = parseFloat(btc_value / valueArray.eth.value).toFixed(4);
+      xlm_value = parseFloat(btc_value / valueArray.xlm.value).toFixed(4);
+      xrp_value = parseFloat(btc_value / valueArray.xrp.value).toFixed(4);
+      document.getElementById('btc_rate').textContent = `1BTC = ${btc_value} USD`;
+      document.getElementById('eth_rate').textContent = `1ETH = ${eth_value} USD`;
+      document.getElementById('xrp_rate').textContent = `1XRP = ${xrp_value} USD`;
+      document.getElementById('xlm_rate').textContent = `1XLM = ${xlm_value} USD`;
     });
   });
 });
